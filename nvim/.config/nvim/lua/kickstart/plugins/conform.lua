c26873c54a41b0cae14b08fbbf6ed13b1705ca46
Kirill Egorov <ke.w0r1t@gmail.com>
2025-01-25 12:28:12 +0500

    Improve golang support

--- nvim/.config/nvim/lua/kickstart/plugins/conform.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/conform.lua
@@ -33,6 +33,7 @@ return {
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				go = { "gofmt" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
